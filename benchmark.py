f7d4030e1f2ffe0e07c48a90213fd2d64b514b3f
MGrass1 <marcus.grass@gmail.com>
2018-09-08 00:18:41 +0200

    code cleanup

--- benchmark.py
+++ benchmark.py
@@ -26,8 +26,6 @@ class Benchmarker(object):
         self.processes.append(multiprocessing.Process(target=self.get_cpp_primes))
         self.processes.append(multiprocessing.Process(target=self.get_java_primes))
 
-        self.user_contine = True
-
         command = input("Enter a ceiling integer for prime search, or 'test' to test functionality: ")
         if command == "test":
             self.test_accuracy()
@@ -39,7 +37,6 @@ class Benchmarker(object):
             self.get_java_primes()
             single_total = time.clock() - single_start
             print("Total time with single-threading %s seconds." % (single_total,))
-            time.sleep(1)
 
             multi_start = time.clock()
             for thread in self.threads:
@@ -48,8 +45,7 @@ class Benchmarker(object):
                 thread.join()
             multi_total = time.clock() - multi_start
             print("Total time with multi-threading %s seconds." % (multi_total,))
-            del self.threads
-            time.sleep(1)
+            del self.threads  # Omitting this causes multiprocessing errors.
 
             process_start = time.clock()
             for process in self.processes:
@@ -58,27 +54,11 @@ class Benchmarker(object):
                 process.join()
             process_total = time.clock() - process_start
             print("Total time with multiprocessing %s seconds" % (process_total,))
-            """
-            print("\nPython, the cpp-library, and the Java Socket Server took %s, %s, and %s seconds to complete" %
-                  (self.py_time, self.cpp_time, self.java_time),
-                  "a prime search of %s, respectively." % self.ceiling)
-            """
-
-    def py_is_prime(self, possible_prime):
-        if possible_prime < 2:
-            return False
-        elif possible_prime == 2:
-            return True
-        else:
-            for denominator in range(2, math.ceil(math.sqrt(possible_prime)) + 1):
-                if possible_prime % denominator == 0:
-                    return False
-        return True
 
     def get_py_primes(self):
         start = time.clock()
         for num in range(self.ceiling):
-            self.py_is_prime(num)
+            py_is_prime(num)
         total = time.clock() - start
         print("\nPython finished in %s seconds." % (total,))
 
@@ -104,7 +84,7 @@ class Benchmarker(object):
         java_primes_found = list()
         python_primes_found = list()
         for num in range(2, ceiling):
-            if self.py_is_prime(num):
+            if py_is_prime(num):
                 python_primes_found.append(num)
             if lib.cpp_is_prime(num):
                 cpp_primes_found.append(num)
@@ -116,6 +96,18 @@ class Benchmarker(object):
 
         assert cpp_primes_found == java_primes_found == python_primes_found
         print("\nAll lists identical, functions successful.\n")
+
+
+def py_is_prime(possible_prime):
+    if possible_prime < 2:
+        return False
+    elif possible_prime == 2:
+        return True
+    else:
+        for denominator in range(2, math.ceil(math.sqrt(possible_prime)) + 1):
+            if possible_prime % denominator == 0:
+                return False
+    return True
 
 
 if __name__ == "__main__":
